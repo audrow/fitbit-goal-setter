28708aef8947589b09fcee7183d0901aa5c14db3
Audrow Nash <audrow@hey.com>
2021-10-24 13:26:49 -0700

    Add list-devices, test-api-keys, and get-status
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,63 @@
 import { load as loadConfiguration } from "./config/index.ts";
 import { makeParser } from "./cli/index.ts";
 import type { Arguments } from "./deps.ts";
+import { getActiveStepTotal, getLastSync } from "./fitbit/index.ts";
+
+const config = await loadConfiguration();
 
 const printArgs = (args: Arguments) => {
   console.log(args);
 };
 
+const listDevices = async (args: Arguments) => {
+  console.log("Devices\n-------")
+  config.fitbit.devices.forEach(device => {
+    console.log(`\t* ${device.name}`)
+  })
+  console.log("\nGo into your config file to adjust devices")
+}
+
+const testApiKeys = async (args: Arguments) => {
+  console.log("Checking...\n")
+  let isError = false;
+  for (const device of config.fitbit.devices) {
+    try {
+      await getLastSync(device.accessToken)
+    } catch (e) {
+      console.error(`Failed to access device '${device.name}': ${e}`)
+      isError = true;
+      continue
+    }
+    try {
+      await getActiveStepTotal(device.accessToken, args.date, config.fitbit.activeSteps)
+    } catch (e) {
+      console.error(`Failed to access intra day steps data for device '${device.name}': ${e}`)
+      isError = true;
+      continue
+    }
+    console.log(`* Device '${device.name}' syncs and has access to intraday steps data`)
+  }
+  if (isError) {
+    console.error("\nPlease fix the errors above and try again")
+  } else {
+    console.log("\nAll devices are syncing and have access to intraday steps data")
+  }
+
+}
+
+const getStatus = async (args: Arguments) => {
+  config.fitbit.devices.forEach(async device => {
+    const activeSteps = await getActiveStepTotal(device.accessToken, args.date, config.fitbit.activeSteps)
+    const lastSyncTime = await getLastSync(device.accessToken)
+    console.log(`${device.name} - ${activeSteps} steps - ${lastSyncTime}`)
+  });
+}
+
 const parser = makeParser({
-  "list-devices": printArgs,
-  "test-api-keys": printArgs,
-  "goal-status": printArgs,
+  "list-devices": listDevices,
+  "test-api-keys": testApiKeys,
+  "goal-status": getStatus,
   "export": printArgs,
   "call-fitbit-api": printArgs,
 });
-// parser(Deno.args);
+parser(Deno.args);
