b48b08e8589da668449c7df78bf2174d2ee4e1fe
Audrow Nash <audrow@hey.com>
2021-10-24 12:14:54 -0700

    Get active steps
    Signed-off-by: Audrow Nash <audrow@hey.com>

---
+++ src/fitbit/activeSteps.test.ts
@@ -0,0 +1,238 @@
+import { assertEquals } from "https://deno.land/std@0.112.0/testing/asserts.ts";
+
+import { getActiveSteps, processSteps } from "./activeSteps.ts";
+import type { ActiveStepsConfig } from "./types.ts";
+
+const testActiveStepsConfig: ActiveStepsConfig = {
+  minDuration: 3,
+  minStepsPerMin: 10,
+  maxInactiveMin: 2,
+};
+
+type StepTestCases = {
+  expected: any;
+  steps: number[];
+  comment?: string;
+}[];
+
+function testActiveSteps(testCases: StepTestCases) {
+  testCases.forEach((testCase) => {
+    assertEquals(
+      testCase.expected,
+      getActiveSteps(testCase.steps, testActiveStepsConfig),
+      testCase.comment,
+    );
+  });
+}
+
+function testProcessSteps(testCases: StepTestCases) {
+  testCases.forEach((testCase) => {
+    assertEquals(
+      testCase.expected,
+      processSteps(testCase.steps, testActiveStepsConfig),
+      testCase.comment,
+    );
+  });
+}
+
+Deno.test({
+  name: "test valid active steps",
+  fn: () => {
+    testActiveSteps(
+      [
+        {
+          expected: 50,
+          steps: [10, 10, 10, 10, 10],
+          comment: "continuous sum to 50",
+        },
+        {
+          expected: 50,
+          steps: [10, 10, 10, 0, 10, 10],
+          comment: "one one minute gap sum to 50",
+        },
+        {
+          expected: 50,
+          steps: [10, 10, 10, 0, 0, 10, 10],
+          comment: "one one two minute gap sum to 50",
+        },
+        {
+          expected: 50,
+          steps: [10, 10, 0, 10, 0, 10, 10],
+          comment: "two one minute gaps sum to 50",
+        },
+        {
+          expected: 50,
+          steps: [10, 10, 0, 0, 10, 0, 0, 10, 10],
+          comment: "two two minute gaps sum to 50",
+        },
+        {
+          expected: 50,
+          steps: [0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0],
+          comment: "many one minute gaps sum to 50",
+        },
+        {
+          expected: 50,
+          steps: [0, 0, 10, 0, 0, 10, 0, 0, 10, 0, 0, 10, 0, 0, 10, 0, 0],
+          comment: "many two minute gaps sum to 50",
+        },
+        {
+          expected: 50,
+          steps: [0, 10, 0, 0, 10, 0, 10, 0, 0, 10, 0, 10, 0, 0],
+          comment: "many variable gaps sum to 50",
+        },
+        {
+          expected: 120,
+          steps: [
+            0,
+            0,
+            9,
+            10,
+            10,
+            0,
+            10,
+            0,
+            0,
+            0,
+            10,
+            9,
+            0,
+            10,
+            10,
+            10,
+            0,
+            0,
+            0,
+            10,
+            10,
+            7,
+            8,
+            10,
+            10,
+            8,
+            9,
+            10,
+          ],
+          comment: "three active step groups with gaps",
+        },
+      ],
+    );
+  },
+});
+
+Deno.test({
+  name: "test invalid active steps",
+  fn: () => {
+    testActiveSteps(
+      [
+        {
+          expected: 0,
+          steps: [10, 10, 0, 0, 0, 10, 10],
+          comment: "too large of a gap with no steps",
+        },
+        {
+          expected: 0,
+          steps: [10, 10, 9, 9, 9, 10, 10],
+          comment: "too large of a gap with less than required steps",
+        },
+      ],
+    );
+  },
+});
+
+Deno.test({
+  name: "process steps into active groups",
+  fn: () => {
+    testProcessSteps([
+      {
+        expected: [[10, 10, 10]],
+        steps: [10, 10, 10],
+        comment: "one continuous active step groups",
+      },
+      {
+        expected: [[10, 10, 10]],
+        steps: [0, 10, 10, 10, 0],
+        comment: "one continuous active step groups with zero padding",
+      },
+      {
+        expected: [[10, 10, 10]],
+        steps: [0, 0, 0, 10, 10, 10, 0, 0, 0, 0],
+        comment: "one continuous active step groups with zero padding",
+      },
+      {
+        expected: [[10, 10, 10]],
+        steps: [10, 0, 10, 10],
+        comment: "one active step groups with one one minute gap",
+      },
+      {
+        expected: [[10, 10, 10]],
+        steps: [0, 10, 0, 10, 0, 10, 0],
+        comment: "one active step groups with many one minute gaps",
+      },
+      {
+        expected: [[10, 10, 10]],
+        steps: [10, 0, 0, 10, 10],
+        comment: "one active step groups with one two minute gap",
+      },
+      {
+        expected: [[10, 10, 10]],
+        steps: [0, 0, 10, 0, 0, 10, 0, 0, 10, 0, 0],
+        comment: "one active step groups with many two minute gap",
+      },
+      {
+        expected: [[10, 10, 10], [10, 10, 10, 10]],
+        steps: [10, 10, 10, 0, 0, 0, 10, 10, 10, 10],
+        comment: "two continuous active step groups",
+      },
+      {
+        expected: [[10, 10, 10], [10, 10, 10, 10]],
+        steps: [10, 10, 10, 9, 9, 9, 10, 10, 10, 10],
+        comment:
+          "two continuous active step groups with almost valid steps in between",
+      },
+      {
+        expected: [[10, 10, 10], [10, 10, 10, 10]],
+        steps: [10, 10, 0, 10, 0, 0, 0, 10, 10, 10, 10],
+        comment: "two active step groups with one gap",
+      },
+      {
+        expected: [[10, 10, 10], [10, 10, 10, 10]],
+        steps: [10, 10, 0, 10, 0, 0, 0, 10, 0, 0, 10, 10, 10],
+        comment: "two active step groups with gaps",
+      },
+      {
+        expected: [[10, 10, 10], [10, 10, 10, 10], [10, 10, 10, 10, 10]],
+        steps: [
+          0,
+          0,
+          9,
+          10,
+          10,
+          0,
+          10,
+          0,
+          0,
+          0,
+          10,
+          9,
+          0,
+          10,
+          10,
+          10,
+          0,
+          0,
+          0,
+          10,
+          10,
+          7,
+          8,
+          10,
+          10,
+          8,
+          9,
+          10,
+        ],
+        comment: "three active step groups with gaps",
+      },
+    ]);
+  },
+});
---
+++ src/fitbit/activeSteps.ts
@@ -0,0 +1,33 @@
+import type { ActiveStepsConfig } from "./types.ts";
+
+export function getActiveSteps(steps: number[], config: ActiveStepsConfig) {
+  const activeSessions = processSteps(steps, config);
+  const activeSessionTotals = activeSessions.map((session) =>
+    session.reduce((acc, curr) => acc + curr, 0)
+  );
+  return activeSessionTotals.reduce((acc, curr) => acc + curr, 0);
+}
+
+export function processSteps(steps: number[], config: ActiveStepsConfig) {
+  let activeSessions: number[][] = [];
+  let activeSession: number[] = [];
+  let consecutiveGap = 0;
+  steps.forEach((step, _) => {
+    if (step >= config.minStepsPerMin) {
+      consecutiveGap = 0;
+      activeSession.push(step);
+    } else {
+      consecutiveGap++;
+    }
+    if (consecutiveGap > config.maxInactiveMin) {
+      if (activeSession.length >= config.minDuration) {
+        activeSessions.push(activeSession);
+      }
+      activeSession = [];
+    }
+  });
+  if (activeSession.length >= config.minDuration) {
+    activeSessions.push(activeSession);
+  }
+  return activeSessions;
+}
